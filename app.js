1b5ba3f5b686c74a30e4b17f3f33784a3e057f98
zhouqing38 <zhouqing38@gmail.com>
2017-06-06 20:34:43 -0400

    add angular-1.6.1

---
+++ app.js
@@ -0,0 +1,8 @@
+angular.module('app',[])
+.controller('MainController',function($scope){
+  $scope.val = "test123";
+});
+
+// var MainController = function($scope){
+//   $scope.val = "test123";
+// }
